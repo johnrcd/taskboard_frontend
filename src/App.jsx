256cc75f26d6ea6deab7a8a7e2df1d7736f066b0
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-05-04 21:41:30 -0500

    created setup for private routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from './util/PrivateRoute';
 import IndexPage from './pages/IndexPage';
 import About from './pages/About';
 import SubmitTask from './pages/SubmitTask';
 import Login from './pages/Login';
 
 export default function App() {
+    // TODO: Auth Service
+    const isLoggedIn = false;
+    
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<IndexPage/>} />
                 <Route path="/about" element={<About/>} />
-                <Route path="/submit" element={<SubmitTask/>} />
-                <Route path="login" element={<Login/>} />
+                <Route path="/submit" element={isLoggedIn ? <SubmitTask/> : <Login/>}/>
+                <Route path="/login" element={<Login/>} />
             </Routes>
         </BrowserRouter>
     )
