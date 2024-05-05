f7df96f8faa61192ebc774e90797a63975f5383a
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-05-04 21:20:34 -0500

    updated TaskList to display loading text when loading

--- src/containers/TaskList.jsx
+++ src/containers/TaskList.jsx
@@ -9,6 +9,7 @@ import TaskOverview from '../components/TaskOverview.jsx';
  */
 const TaskList = ({onTaskClick, className}) => {
     const [tasks, setTasks] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     // https://react.dev/reference/react/useEffect#fetching-data-with-effects
     useEffect(() => {
@@ -18,6 +19,7 @@ const TaskList = ({onTaskClick, className}) => {
             .then(data => {
                 if (!ignore) {
                     setTasks(data);
+                    setIsLoaded(true);
                 }
             });
 
@@ -31,15 +33,20 @@ const TaskList = ({onTaskClick, className}) => {
             className + " " +
             "flex flex-col space-y-2"
         }>
-        {
-            tasks.map((task, index) =>
-                <TaskOverview
-                    uuid={task.uuid}
-                    summary={task.summary}
-                    onTaskClick={(uuid) => onTaskClick(uuid)}
-                    key={"TaskList_task_" + index}
-                />
-            )
+        {isLoaded &&
+        tasks.map((task, index) =>
+            <TaskOverview
+                uuid={task.uuid}
+                summary={task.summary}
+                onTaskClick={(uuid) => onTaskClick(uuid)}
+                key={"TaskList_task_" + index}
+            />
+        )
+        }
+        {!isLoaded && 
+        <p className="text-white text-normal font-normal m-0 p-0 line-clamp-1">
+            Loading...
+        </p>
         }
         </div>
     )
