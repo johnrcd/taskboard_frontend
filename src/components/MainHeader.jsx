9a70c8bf60f4f911ef1e28698de5dcfd2768332c
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-05-07 21:28:17 -0500

    made nav bar mobile friendly

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,27 +1,104 @@
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const MainHeader = () => {
-    return(
-        <header className="pt-5 pb-5 flex flex-col justify-center">
-            <h1 className="flex-1 text-4xl md:text-6xl font-bold text-center text-stone-100">
-                The Taskboard
-            </h1>
-            <p className="flex-1 text-base font-normal text-center text-stone-300 mt-2">Created by Rovi Decena</p>
-            <nav className="
-                text-base font-normal text-center text-stone-300
-                mt-2 p-2 flex-1
-                bg-gradient-to-r from-cyan-500/10 to-blue-500/10
-                rounded-lg
-            ">
-                <ul className="flex justify-center divide-x divide-stone-300/30">
-                    <li><Link className="px-3 hover:underline hover:text-stone-100" to="/about">About</Link></li>
-                    <li><Link className="px-3 hover:underline hover:text-stone-100" to="/">View Board</Link></li>
-                    <li><Link className="px-3 hover:underline hover:text-stone-100" to="/submit">Submit A Task</Link></li>
-                    <li><Link className="px-3 hover:underline hover:text-stone-100" to="/login">Login</Link></li>
-                    <li className="px-3 hover:underline hover:text-stone-100">Register</li>
-                </ul>
-            </nav>
-        </header>
+    // State to manage the navbar's visibility
+    const [isNavigationEnabled, setIsNavigatorEnabled] = useState(false);
+
+    // Toggle function to handle the navbar's display
+    const handleNav = () => {
+        setIsNavigatorEnabled(!isNavigationEnabled);
+    };
+
+    const navigationLinks = [
+        { id: 1, path: "/about",  text: "About"         },
+        { id: 2, path: "/",       text: "View Board"    },
+        { id: 3, path: "/submit", text: "Submit A Task" },
+        // { id: 4, path: "/login",  text: "Login"         },
+    ];
+
+    return (
+        <>
+            <header className="pt-5 pb-5 hidden md:flex flex-col justify-center">
+                <h1 className="flex-1 text-4xl md:text-6xl font-bold text-center text-stone-100">
+                    The Taskboard
+                </h1>
+                <p className="flex-1 text-base font-normal text-center text-stone-300 mt-2">Created by Rovi Decena</p>
+
+                {/* Desktop Navigation */}
+
+                <nav className="
+                    text-base font-normal text-center text-stone-300
+                    mt-2 p-2 flex-1
+                    bg-gradient-to-r from-cyan-500/10 to-blue-500/10
+                    rounded-lg
+                    hidden md:block
+                ">
+                    <ul className="hidden md:flex justify-center divide-x divide-stone-300/30">
+                    {navigationLinks.map(item => (
+                        <li key={item.id}>
+                            <Link
+                                className="px-3 hover:underline hover:text-stone-100"
+                                to={item.path}
+                            >
+                                {item.text}
+                            </Link>
+                        </li>
+                    ))}
+                    </ul>
+                </nav>
+            </header>
+
+            {/* Mobile Navigation Menu */}
+            <header className="">
+                <nav className="
+                    text-base font-normal text-center text-stone-300
+                    p-2 flex-1
+                    m-0
+
+                    fixed md:hidden
+                    right-0 top-0
+                    bg-gray-900
+                    w-full
+                ">
+                    <div className="flex flex-row justify-around">
+                        <h1 className="flex-1 text-2xl md:text-6xl font-bold text-left text-stone-100 inline">
+                            The Taskboard
+                        </h1>
+
+                        <div onClick={handleNav} className="flex-none">
+                            {isNavigationEnabled ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+                        </div>
+                    </div>
+
+                    <ul
+                        className={
+                            isNavigationEnabled
+                            ? 'block md:hidden right-0 top-0 w-full border-r border-r-gray-900 bg-slate-900'
+                            : 'h-[0%] fixed bottom-0 left-[-100%] top-[-100%]'
+                        }
+                    >
+                    {navigationLinks.map(item => (
+                        <li key={item.id}>
+                            <Link
+                                className="px-3 hover:underline hover:text-stone-100"
+                                to={item.path}
+                            >
+                                {item.text}
+                            </Link>
+                        </li>
+                    ))}
+                    </ul>
+                </nav>
+            </header>
+
+            {/* how do you properly prevent a fixed nav bar from overlapping... */}
+            <div className="block md:hidden">
+                <br /> 
+                <br />
+            </div>
+        </>
     )
 };
 
