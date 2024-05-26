0d2d4d816b62c945d12a1f657856743374c23a5d
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-05-25 22:20:23 -0500

    cleaned up routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import IndexPage from './pages/IndexPage';
 import About from './pages/About';
 import SubmitTask from './pages/SubmitTask';
 import Login from './pages/Login';
-import PostComment from './pages/PostComment';
+import Register from './pages/Register';
 
 export default function App() {    
     return (
         <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
-                <Route path="/" element={<IndexPage />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/submit" element={<PrivateRoute component={<SubmitTask />}/>}/>
-                <Route path="/comment" element={<PrivateRoute component={<PostComment />}/>}/>
-                <Route path="/login" element={<Login />} />
+                <Route path="/"         element={<IndexPage />} />
+                <Route path="/about"    element={<About />} />
+                <Route path="/submit"   element={<PrivateRoute component={<SubmitTask />}/>}/>
+                <Route path="/login"    element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "../components/LoginForm";
+import LoginContainer from "../containers/LoginContainer";
+
+const Register = () => {
+    return (
+        <div className="bg-slate-900 min-h-screen flex justify-center">
+            <div className="flex align-center flex-col justify-center">
+                <LoginContainer />
+            </div>
+        </div>
+    )
+};
+
+export default Register;
