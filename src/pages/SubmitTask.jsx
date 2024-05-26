90a0681d01506e5fe2d0c40f7e950e5a93fbd255
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-05-25 22:21:42 -0500

    minor style change

--- src/pages/SubmitTask.jsx
+++ src/pages/SubmitTask.jsx
@@ -5,7 +5,7 @@ import SubmitTaskContainer from "../containers/SubmitTaskContainer";
 const SubmitTask = () => {
     return (
         <div className="bg-slate-900 min-h-screen flex justify-center">
-            <div className="w-full max-w-7xl text-slate-200 ml-2 mr-2">
+            <div className="w-full max-w-7xl text-slate-200 ml-2 mr-2 mb-4">
                 <MainHeader />
                 <div className="flex justify-center flex-col">
                     <SubmitTaskContainer />
