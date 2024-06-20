e8651dcfd7f06eca54146fda4a52059406692cbb
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-06-20 01:41:15 -0500

    added colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
     content: ["./src/**/*.jsx"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    text: "#ffffff",
+                    background: "#ffffff",
+                    accent: "#ffffff",
+                    button: {
+                        text: "#ffffff",
+                        background: "#ffffff",
+                        hover: "#ffffff",
+                        selected: "#ffffff",
+                    },
+                },
+                overlay: {
+                    text: "#ffffff",
+                    background: "#ffffff",
+                    accent: "#ffffff",
+                    button: {
+                        text: "#ffffff",
+                        background: "#ffffff",
+                        hover: "#ffffff",
+                        selected: "#ffffff",
+                    },
+                },
+            },
+        },
         container: {
             center: true,
         },
