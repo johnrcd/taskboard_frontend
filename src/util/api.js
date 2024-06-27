2d37306da18912a027d19fd77ef17813e24dcf75
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-06-26 22:07:26 -0500

    cleaned up api.js

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,14 @@
 import { getAccessToken } from "./auth";
-// swap root_url depending on where the REST API is being hosted
-// don't ask why one url is being exported i forgot
+
+// root_url is swapped depending on what backend i'm using (prod vs local)
+
+// q: wouldn't it be better to use enviornmental variables when running
+//    locally rather than changing a variable that keeps getting committed to
+//    the repo
+// a: yeah probably
+
+// q: why is one of the urls exported but the other isn't
+// a: idk i forgot
 
 // export const root_url = "http://127.0.0.1:8000"
 const root_url = "https://johnrcd.pythonanywhere.com"
@@ -8,9 +16,9 @@ const root_url = "https://johnrcd.pythonanywhere.com"
 /**
  * Wrapper for fetch request.
  * @param {string} path The API endpoint you are trying to access. Expects
- * a leading slash. (e.g. /api/tasks/ )
- * @param {*} options Options for fetch request. If this is null, fetch will
- * be treated as a get request.
+ * a leading slash. (e.g. /api/tasks/)
+ * @param {object} options Options for fetch request. If this is null, fetch
+ * will be treated as a get request.
  * @returns {Promise<Response>} Response from fetch.
  */
 export const fetchFromApi = async(path, options) => {
@@ -27,10 +35,10 @@ export const fetchFromApi = async(path, options) => {
 
 /**
  * Adds credentials to the options in a fetch request.
- * @param {string} path The API endpoint you are trying to access. Do not put
- * in the domain. This string should not start with a slash.
+ * @param {string} path The API endpoint you are trying to access. Expects
+ * a leading slash. (e.g. /api/tasks/)
  * @param {object} options Options for fetch request. If the Authorization
- * header is set, it will be overriden. This must be set.
+ * header is set, it will be overridden. This must be set.
  * @returns Response from fetch.
  */
 export const fetchAsUser = async(path, options) => {
