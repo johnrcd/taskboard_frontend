d009d0df8ac53e7285d855b6163c450f8bf1ecdd
johnrcd <80877409+johnrcd@users.noreply.github.com>
2024-08-06 18:13:49 -0500

    added profile pictures to task comments

--- src/components/TaskComment.jsx
+++ src/components/TaskComment.jsx
@@ -2,31 +2,47 @@ import ReactTimeAgo from 'react-time-ago'
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
+import Identicon from "../components/Identicon";
+
 const TaskComment = ({poster, dateCreated, content, keyValue}) => {
     const date = new Date(dateCreated);
-    const navigate = useNavigate();
     
     return (
         <article
+            className="
+                flex flex-row
+            "
             key={keyValue}
         >
-            <h4 className="
-                text-primary-text
-                text-normal hover:underline
-                font-bold
-                tracking-tight inline-block
-            ">
-                <Link to={"/profile?username=" + poster}>{poster}</Link>
-            </h4>
-            <p
-                className="text-primary-tooltip text-sm pl-2 tracking-tight inline-block"
-                title={date.toLocaleString()}
-            >
-                {<ReactTimeAgo date={date} locale="en-US"/>}
-            </p>
-            <p className="text-primary-text text-normal tracking-tight max-w-xl break-words">
-                {content}
-            </p>
+            <aside>
+                <Identicon username={poster} width="50" height="50"/>
+            </aside>
+            <div>
+                <h4 className="
+                    text-primary-text
+                    text-normal hover:underline
+                    font-bold
+                    tracking-tight inline-block
+                ">
+                    <Link to={"/profile?username=" + poster}>{poster}</Link>
+                </h4>
+                <p
+                    className="
+                        text-primary-tooltip text-sm
+                        pl-2 tracking-tight
+                        inline-block
+                    "
+                    title={date.toLocaleString()}
+                >
+                    {<ReactTimeAgo date={date} locale="en-US"/>}
+                </p>
+                <p className="
+                    text-primary-text text-normal tracking-tight
+                    max-w-xl break-words
+                ">
+                    {content}
+                </p>
+            </div>
         </article>
     )
 };
--- src/components/TaskOverview.jsx
+++ src/components/TaskOverview.jsx
@@ -3,18 +3,25 @@ const TaskOverview = ({uuid, summary, onTaskClick}) => {
         <button className="
             w-full
             flex flex-col justify-between
-            bg-primary-background hover:bg-primary-border
+            bg-primary-background
+            hover:bg-primary-border
+            active:bg-primary-accent
             px-4 py-3
         "
             onClick={() => onTaskClick(uuid)}
         >
             <div className="grow flex flex-col text-left">
-                <p className="text-primary-tooltip text-sm tracking-tight line-clamp-1"
+                <p className="
+                    text-primary-tooltip text-sm tracking-tight line-clamp-1
+                "
                     title={"uuid: " + uuid}
                 >
                     uuid: {uuid}
                 </p>
-                <h3 className="text-primary-text text-normal font-normal m-0 p-0 line-clamp-1"
+                <h3 className="
+                    text-primary-text text-normal font-normal line-clamp-1
+                    m-0 p-0
+                "
                     title={summary}
                 >
                     {summary}
