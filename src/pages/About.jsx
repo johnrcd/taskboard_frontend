346cf2293ff99e69ddcd0b518fab16c8b43ab2ac
johnrcd <80877409+johnrcd@users.noreply.github.com>
2025-01-17 15:01:36 -0600

    chore: change about and config for cloudflare

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,25 +14,18 @@ const About = () => {
                     want added, note down any issues that you find, or give ideas for new
                     projects.
                 </p> <br />
-                <p> 
+                <p>
                     By default, everyone has access to view the Taskboard. However, if you want
                     to interact with the site, you'll need to register an account. This will
                     give you the ability to submit tasks and post comments.
                 </p> <br />
-                <hr className="border-1 border-slate-500"/> <br />
-                <p> 
-                    As it stands, this website can be considered complete. I have implemented
-                    all the major features I sought out to when I initially planned this
-                    website. However, there's always room for improvement, so I may periodically
-                    update this website whenever I have time.
-                </p> <br />
-                <p> 
+                <p>
                     NOTE: This website has been created for demonstration purposes. Feel free
                     to use this website (I encourage it!), but keep in mind I won't be actively
                     checking this website out and actually processing tickets.
                 </p> <br />
                 <hr className="border-1 border-slate-500"/> <br />
-                <p> 
+                <p>
                     Created by Rovi Decena.
                 </p> <br />
             </main>
